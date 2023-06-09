7c6a51bb5f05e3d5518d7650f625945d9e9ba5da
Matthew McIvor <matthewrmcivor@gmail.com>
2023-06-08 20:01:26 -0700

    Added redis Caching for dramatically impoved performance

--- server/index.js
+++ server/index.js
@@ -2,10 +2,43 @@ const express = require('express');
 const path = require('path');
 const port = 3001;
 const { getProducts, getProduct, getStyles, getRelated } = require('../database/postgresDB');
+const redis = require('redis');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
+app.use(cache);
+
+
+// Create a Redis client
+const client = redis.createClient({
+  host: "3.139.105.83",
+  port: 6379,
+});
+
+function cache(req, res, next) {
+  const key = "__express__" + req.originalUrl || req.url;
+
+  client.get(key).then(reply => {    
+    if (reply) {
+      res.send(JSON.parse(reply));
+    } else {
+      res.sendResponse = res.send;
+      res.send = (body) => {
+        //expire in 1 min
+        // console.log('body====> ', body);
+        client.set(key, JSON.stringify(body), {'EX':60});
+        res.sendResponse(body);
+      };
+      next();
+    }
+  }).catch(err=>{
+    console.log(err);
+    res.status(500).send(err)
+  });
+}
+
+
 
 
 console.log(__dirname);
@@ -34,12 +67,23 @@ app.get('/products', (req, res) => {
 
 
 
-
 // Product Information
 // GET /products/:product_id
 // Returns all product level information for a specified product id.
 // Params: product_id
+// app.get('/product', (req, res) => {
+//   console.log("req.query.product_id===> ", req.query.product_id);
+//   getStyles(parseInt(req.query.product_id))
+//   .then(data => {
+//     res.status(200).send(data.rows[0])
+//   })
+//   .catch((err) => {
+//     res.sendStatus(404)
+//   });
+// });
+
 app.get('/product', (req, res) => {
+  // console.log("req.query.product_id===> ", req.query.product_id);
   getStyles(parseInt(req.query.product_id))
   .then(data => {
     res.status(200).send(data.rows[0])
@@ -48,6 +92,9 @@ app.get('/product', (req, res) => {
     res.sendStatus(404)
   });
 });
+
+
+
 
 // Product Styles
 // GET /products/:product_id/styles
@@ -80,6 +127,9 @@ app.get('/related', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
+  client.connect().then(()=> {
+    console.log('redis is connected')
+  })
 });
 
 module.exports = app;
